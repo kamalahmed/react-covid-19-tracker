ef5b57e8c4993bbdbf4c007713ff697029e15413
Kamal Ahmed <kamalacca@gmail.com>
2020-04-14 05:26:43 +0600

    added function to fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react'
 import { Cards, Chart, CountryPicker } from "./Components";
 import styles from './App.module.css'
-
+import { fetchData } from "./API";
 
 
 
 class App extends Component {
+  state = {
+    data : {},
+  }
+  async componentDidMount(){
+    const dataFromAPI = await fetchData();
+    //console.log('Log response Data', dataFromAPI);
+    this.setState({
+      data: dataFromAPI
+    });
+    
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data}/>
         <CountryPicker />
         <Chart />
       </div>
